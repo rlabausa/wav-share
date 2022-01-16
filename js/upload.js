597bcb332746ebaaacd4bf2426a5d23b7db835c6
rlabausa <rubylabausa@tgmail.com>
2022-01-16 13:22:03 -0600

    Encode .wav file as Base64 string

--- js/upload.js
+++ js/upload.js
@@ -10,12 +10,38 @@ function clearFileSelection(event) {
 function handleFileSelection(event) {
     const file = event.target.files[0];
     const span = document.querySelector('span.input-file-selection');
-    span.textContent = file.name;
+    if (file && file.type === 'audio/wav') {
+        span.textContent = file.name;
+
+        const reader = new FileReader();
+
+        reader.onload = function () {
+            const resultString = reader.result;
+
+            // Remove Data-URL declaration (data:audio/wav;base64,) 
+            // This allows blob to be directly decoded as Base64
+            const base64String = resultString.replace(/^.*\/.*,/, '');
+
+            const hiddenField = document.querySelector('#fileBase64Encoding');
+            hiddenField.value = base64String;
+        }
+
+        reader.onerror = function () {
+            span.textContent = 'Error occurred while reading file.';
+        }
+
+        // Perform the read to trigger the FileReader's `load` event
+        reader.readAsDataURL(file);
+    } else if (file) {
+        span.textContent = 'Invalid file selection.  Only .wav files are accepted.';
+    } else {
+        span.textContent = 'No file chosen';
+    }
 }
 
 window.onload = function () {
     const fileInput = document.querySelector('#file');
-    
+
     fileInput.addEventListener('change', handleFileSelection);
     fileInput.addEventListener('click', clearFileSelection);
 
